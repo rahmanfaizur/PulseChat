b64e45f72afb31667641c60ae251b504a28293ff
rahmanfaizur <rahman.faizur.fr@gmail.com>
2026-02-25 04:54:21 +0530

    fix(auth): hardcode clerk jwt issuer domain in convex config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+            domain: "https://precious-goshawk-30.clerk.accounts.dev",
             applicationID: "convex",
         },
     ],
